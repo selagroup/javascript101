4e94973064116794dfd253cdcb256c757b5e3a65
Nir Noy <nirn@sela.co.il>
2017-06-15 13:20:32 +0300

    lab

--- module04-advanced_func/demos/script.js
+++ module04-advanced_func/demos/script.js
@@ -19,6 +19,54 @@
 // Lecture: Closures
 
 
-
+*/
 /////////////////////////////
 // Lecture: Bind, call and apply
+
+let intervalCounter= (function(){
+    
+    let countStart = 0;
+    let countEnd;
+    let interval_cb;
+    let interval_cb_context;
+    
+    function initCounter(start,end,onInterval,onIntervalContext){
+
+        countStart=start;
+        countEnd=end;
+        interval_cb = onInterval;
+        interval_cb_context=onIntervalContext;
+        timeout();
+    }
+    
+    function timeout(){
+        
+
+        setTimeout(()=>{
+            countStart++;
+            interval_cb.call(interval_cb_context,countStart);
+            if(countStart<countEnd){
+                timeout();
+            }
+
+        },1000 )
+
+    }
+    return initCounter;
+}());
+
+let myCount={
+    currentCount:0,
+    start:function(){
+        intervalCounter(0,5,this.increment,this);
+    },
+    increment:function(count){
+        console.log(count);
+        this.currentCount=count;
+    }
+}
+
+myCount.start();
+setTimeout(()=>{
+    console.log(myCount.currentCount);
+},6000)
---
+++ module06-async_javascript/lab2_Node/solution/asyncFunc.js
@@ -0,0 +1,45 @@
+
+module.exports.parallel = (promises) =>{
+    return Promise.all(promises)
+}
+async function series(funcs){
+    
+    let result=[];
+    for(i=0; i<funcs.length; i++){
+        result.push(await funcs[i]());
+    };
+    return result;   
+}
+
+let id=0;
+function RandomTimer(){
+    
+    id++
+    let rand=(Math.random() * (5 - 1) + 1)*1000;
+    console.log(rand);
+    return new Promise((resolve,reject)=>{
+        setTimeout((id)=>{
+            console.log(id);
+            resolve(id);            
+        },rand,id);
+    });
+}
+
+
+//  series([
+//         RandomTimer,
+//         RandomTimer,
+//         RandomTimer
+//     ]).then((results)=>{
+//         console.log(results);
+//     },(err)=> console.log(err));
+
+
+
+seriesPromise([
+    RandomTimer,
+    RandomTimer,
+    RandomTimer
+]).then((result)=>{
+    console.log(result);
+})
